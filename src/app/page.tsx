c80f5313b731589a111634f35c732a19b6d0b572
vrajdesai78 <vrajdesai78@gmail.com>
2023-06-22 13:33:12 +0530

    Updated gitignore

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,41 @@
+'use client';
+
 export default function Home() {
+
+  const createRoom = async () => {
+    const res = await fetch("https://api.huddle01.com/api/v1/create-room", {
+      method: "POST",
+      body: JSON.stringify({
+        title: "Test Room",
+      }),
+      headers: {
+        "Content-Type": "application/json",
+        'x-api-key': process.env.API_KEY ?? ""
+      }
+    })
+    const data = await res.json()
+    console.log(data)
+  }
+
   return (
     <main className="flex h-screen relative items-center justify-center bg-lobby text-slate-100">
-      Hello
+      <div className="flex columns-1 items-center gap-2">
+        <img src="/images/avatar.png" alt="avatar" />
+        {/* Add */}
+        <div className="flex items-center">
+          <div className="flex flex-col items-center justify-center">
+            Set a display name
+            <input
+              className="border-2 border-slate-100 rounded-md p-2 mt-2"
+              type="text"
+              placeholder="Display name"
+            />
+            <button className="bg-slate-100 text-slate-900 rounded-md p-2 mt-2" onClick={createRoom}>
+              Start Spaces
+            </button>
+          </div>
+        </div>
+      </div>
     </main>
   );
 }

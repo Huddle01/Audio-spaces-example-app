a7c1a635eb7a55b69e9a571f4cf25e371dcdf37c
Yash Chowdhury <yashchowdhurysn@gmail.com>
2023-10-21 03:52:48 +0530

    resolved username issue, made ui better

--- src/assets/BasicIcons.tsx
+++ src/assets/BasicIcons.tsx
@@ -281,4 +281,30 @@ export const BasicIcons = {
       />
     </svg>
   ),
+  send: (
+    <svg
+      width="32px"
+      height="32px"
+      viewBox="-2.4 -2.4 28.80 28.80"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      stroke="#000000"
+      stroke-width="0.00024000000000000003"
+    >
+      <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+      <g
+        id="SVGRepo_tracerCarrier"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      ></g>
+      <g id="SVGRepo_iconCarrier">
+        <path
+          fill-rule="evenodd"
+          clip-rule="evenodd"
+          d="M1.265 4.42619C1.04293 2.87167 2.6169 1.67931 4.05323 2.31397L21.8341 10.1706C23.423 10.8727 23.423 13.1273 21.8341 13.8294L4.05323 21.686C2.6169 22.3207 1.04293 21.1283 1.265 19.5738L1.99102 14.4917C2.06002 14.0087 2.41458 13.6156 2.88791 13.4972L8.87688 12L2.88791 10.5028C2.41458 10.3844 2.06002 9.99129 1.99102 9.50829L1.265 4.42619ZM21.0257 12L3.2449 4.14335L3.89484 8.69294L12.8545 10.9328C13.9654 11.2106 13.9654 12.7894 12.8545 13.0672L3.89484 15.3071L3.2449 19.8566L21.0257 12Z"
+          fill="#ffffff"
+        ></path>
+      </g>
+    </svg>
+  ),
 };
--- src/components/Chat/Chat.tsx
+++ src/components/Chat/Chat.tsx
@@ -3,16 +3,19 @@ import React from 'react';
 import useChatScroll from './ChatScroll';
 import { useAppUtils } from '@huddle01/react/app-utils';
 import { nanoid } from 'nanoid';
+import useStore from '@/store/slices';
 import { useState, useRef } from 'react';
 import { useEventListener } from '@huddle01/react/hooks';
-import { usePeers } from '@huddle01/react/hooks';
+import { BasicIcons } from '@/assets/BasicIcons';
 
 const Chat = () => {
-  const [message, setMessage] = useState('');
-  const [chats, setChats] = useState<{ text: string; is_user: boolean }[]>([]);
+  const userDisplayName = useStore((state) => state.userDisplayName);
+  const [message, setMessage] = useState<string>('');
+  const [chats, setChats] = useState<
+    { name: string; text: string; is_user: boolean }[]
+  >([]);
   const ref = useChatScroll(chats);
-  const buttonRef = useRef(null);
-  const [peerId, setPeerId] = useState('');
+  const buttonRef = useRef<HTMLButtonElement | null>(null);
   const { sendData } = useAppUtils();
 
   async function handleSend() {
@@ -21,6 +24,7 @@ const Chat = () => {
       return [
         ...chats,
         {
+          name: userDisplayName,
           text: message,
           is_user: true,
         },
@@ -29,26 +33,26 @@ const Chat = () => {
     setMessage('');
   }
 
-  const handleKeyDown = (event: any) => {
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
     if (event.key === 'Enter') {
       buttonRef.current?.click();
     }
   };
 
-  function handleChange(event: any) {
+  function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
     setMessage(event.target.value);
   }
 
   const sendDataToAllPeers = () => {
-    sendData('*', { message: message });
+    sendData('*', { message: message, name: userDisplayName });
   };
 
   useEventListener('room:data-received', (data) => {
-    setPeerId(data.fromPeerId);
     setChats((chats) => {
       return [
         ...chats,
         {
+          name: data.payload.name,
           text: data.payload.message,
           is_user: false,
         },
@@ -56,52 +60,42 @@ const Chat = () => {
     });
   });
 
-  const peer = usePeers(peerId);
-  const name = peer?.peers[peerId]?.displayName;
-
   const displayChats = chats.map((chat) => {
     return (
       <div
         key={nanoid()}
         className={`${
           chat.is_user
-            ? 'ml-auto mr-2 text-md break-words shadow-md max-w-md w-fit py-2 px-2 mb-2 bg-blue-600 rounded-xl'
-            : 'w-fit py-2 px-2 break-words max-w-md shadow-md text-md mb-2 ml-2 rounded-xl bg-slate-700'
+            ? 'ml-auto text-md break-words shadow-md max-w-xs w-fit py-1 px-4 mb-2 bg-[#216CFC] rounded-2xl'
+            : 'w-fit py-1 px-4 break-words max-w-xs shadow-md text-md mb-2 rounded-2xl bg-[#242731]'
         }`}
       >
         <div className="text-xs text-blue-300">
-          {chat.is_user ? null : name}
+          {chat.is_user ? null : chat.name}
         </div>
-
         {chat.text}
       </div>
     );
   });
 
   return (
-    <div className="text-white w-1/3 h-3/4 p-2">
+    <div className="text-white w-1/4 h-4/5 p-2 mr-3">
       <div className="flex flex-col h-full">
-        <div className="mb-6 pl-2 font-mono text-left text-3xl ">
-          Huddle Chat
-        </div>
+        <div className="mb-6 font-mono text-left text-2xl ">Huddle Chat</div>
         <div ref={ref} className="overflow-auto flex-col h-full">
           <div className="font-sans">{displayChats}</div>
         </div>
-        <div className="flex py-2 pl-2">
+        <div className="flex py-2">
           <input
             type="text"
-            placeholder="What do you want to say?"
-            className="p-2 rounded-xl w-full bg-slate-600"
+            placeholder="Type a message"
+            className="p-1.5 rounded-xl w-full bg-[#242731]"
             value={message}
             onChange={handleChange}
             onKeyDown={handleKeyDown}
           />
-          <button
-            ref={buttonRef}
-            className="text-xl p-2 text-white hover:text-blue-500"
-            onClick={handleSend}
-          >
-            Send
+          <button ref={buttonRef} className="p-1" onClick={handleSend}>
+            {BasicIcons.send}
           </button>
         </div>
       </div>
